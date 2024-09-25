bc3e1dc41be6aa6ae4b37e7a6ca2fe5aa0ab9157
igorocky <123@gmail.com>
2024-09-25 11:02:42 +0200

    Update conveyor.

--- src/conveyor/conveyor.py
+++ src/conveyor/conveyor.py
@@ -4,8 +4,9 @@ from typing import Callable
 
 import pygame.draw
 from pygame import Rect, Surface, SurfaceType
+from pygame.font import Font
 
-from common import BLACK, WHITE, PASTEL_RED, PALE_GREEN
+from common import BLACK, WHITE, PASTEL_RED, RED, PALE_GREEN
 
 
 class Box:
@@ -41,7 +42,8 @@ class Conveyor:
                  velocity_x_px_sec: float, velocity_y_px_sec: float,
                  box_origin_x: float, box_origin_y: float,
                  focus_point_x: float, focus_point_y: float,
-                 draw_focus_line:Callable[[Surface | SurfaceType], None]) -> None:
+                 draw_focus_line: Callable[[Surface | SurfaceType], None],
+                 font: Font) -> None:
         self._created_at_sec = curr_time_sec
         self._conv_rect = conv_rect
         self._min_delay_sec = min_delay_sec
@@ -56,6 +58,8 @@ class Conveyor:
         self._focus_point_x = focus_point_x
         self._focus_point_y = focus_point_y
         self._draw_focus_line = draw_focus_line
+        self._error_cnt = 0
+        self._font = font
 
     def _get_random_delay(self) -> float:
         return random.uniform(self._min_delay_sec, self._max_delay_sec)
@@ -88,6 +92,13 @@ class Conveyor:
             elif box.is_focused:
                 box.is_focused = False
                 box.is_failed = True
+                self._error_cnt += 1
+
+    def remove_focused_boxes(self) -> None:
+        old_boxes = self._boxes
+        self._boxes = [box for box in self._boxes if not box.is_focused]
+        if len(old_boxes) == len(self._boxes):
+            self._error_cnt += 1
 
     def render(self, disp: Surface | SurfaceType) -> None:
         pygame.draw.lines(
@@ -99,6 +110,12 @@ class Conveyor:
                 (self._conv_rect.left, self._conv_rect.bottom),
             ]
         )
+        text_surf = self._font.render(str(self._error_cnt), False, RED)
+        text_surf = pygame.transform.scale_by(text_surf, self._conv_rect.height / text_surf.get_height())
+        text_rect = text_surf.get_rect()
+        text_rect.right = self._conv_rect.left - 50
+        text_rect.top = self._conv_rect.top
+        disp.blit(text_surf, text_rect)
         for box in self._boxes:
             color = PASTEL_RED if box.is_failed else PALE_GREEN if box.is_focused else BLACK
             pygame.draw.circle(disp, color, (box.curr_pos_x, box.curr_pos_y), self._box_radius)
--- src/conveyor/main.py
+++ src/conveyor/main.py
@@ -3,6 +3,7 @@ import time
 
 import pygame
 from pygame import Surface, SurfaceType, Rect
+from pygame.font import Font
 
 from common import GRAY, BLUE
 from conveyor import Conveyor
@@ -15,53 +16,66 @@ pygame.init()
 FPS = 30
 fpsClock = pygame.time.Clock()
 
+pygame.font.init()
+font: Font = pygame.font.SysFont('Comic Sans MS', 30)
+
 DISP: Surface | SurfaceType = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Conveyor')
 
 
 def draw_focus_line(disp: Surface | SurfaceType, conv_rect: Rect, focus_point_x: float) -> None:
-    pygame.draw.line(disp, BLUE, (focus_point_x, conv_rect.top), (focus_point_x, conv_rect.bottom))
+    pygame.draw.line(disp, BLUE, (focus_point_x, conv_rect.top), (focus_point_x, conv_rect.bottom), width=3)
 
-
-def main() -> None:
-    conv_rect: Rect = Rect(0, 0, 800, 50)
-    conv_rect.center = (int(WINDOW_WIDTH / 2), int(WINDOW_HEIGHT / 2))
+def make_conveyor(conv_rect: Rect) -> Conveyor:
     box_origin_x = conv_rect.right - 1
     box_origin_y = (conv_rect.top + conv_rect.bottom) / 2
     focus_point_x: float = conv_rect.left + 0.2 * conv_rect.width
-    conveyor = Conveyor(
+    return Conveyor(
         curr_time_sec=time.time(),
         conv_rect=conv_rect,
         min_delay_sec=0.2,
         max_delay_sec=1,
         box_radius=20,
-        velocity_x_px_sec=-300,
+        velocity_x_px_sec=-200,
         velocity_y_px_sec=0,
         box_origin_x=box_origin_x,
         box_origin_y=box_origin_y,
         focus_point_x=focus_point_x,
         focus_point_y=box_origin_y,
-        draw_focus_line=lambda disp: draw_focus_line(disp, conv_rect, focus_point_x)
+        draw_focus_line=lambda disp: draw_focus_line(disp, conv_rect, focus_point_x),
+        font=font,
     )
+
+GameState = dict[int,Conveyor]
+
+def main() -> None:
+    game_state: GameState = {}
+
+    upper_conv_rect: Rect = Rect(0, 0, 800, 50)
+    upper_conv_rect.center = (int(WINDOW_WIDTH / 2), int(WINDOW_HEIGHT / 2))
+    game_state[pygame.K_j] = make_conveyor(upper_conv_rect)
     while True:
         curr_time_sec = time.time()
-        conveyor.update(curr_time_sec)
-        handle_events(conveyor)
-        render(conveyor)
+        for conv in game_state.values():
+            conv.update(curr_time_sec)
+        handle_events(game_state)
+        render(game_state)
 
 
-def handle_events(conveyor: Conveyor) -> None:
+def handle_events(game_state: GameState) -> None:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        # elif event.type == pygame.MOUSEBUTTONDOWN:
-        #     state.handle_click(event.pos)
+        elif event.type == pygame.KEYDOWN:
+            if event.key in game_state:
+                game_state[event.key].remove_focused_boxes()
 
 
-def render(conveyor: Conveyor) -> None:
+def render(game_state: GameState) -> None:
     DISP.fill(GRAY)
-    conveyor.render(DISP)
+    for conv in game_state.values():
+        conv.render(DISP)
     pygame.display.update()
     fpsClock.tick(FPS)
     # print(f'{fpsClock.get_fps()=}')
