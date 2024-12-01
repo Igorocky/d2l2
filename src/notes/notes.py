6bed353387eeee2c3809e09d78d8df468a4930aa
igorocky <123@gmail.com>
2024-12-01 11:02:59 +0100

    Update notes.py.

--- src/notes/notes.py
+++ src/notes/notes.py
@@ -15,12 +15,14 @@ pygame.init()
 FPS = 30
 fpsClock = pygame.time.Clock()
 
-DISP: Surface | SurfaceType = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+# DISP: Surface | SurfaceType = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+DISP: Surface | SurfaceType = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption('Notes')
 
 
 def main() -> None:
-    game_manager = GameManager(window_width=WINDOW_WIDTH, window_height=WINDOW_HEIGHT, db_file_path=sys.argv[1])
+    # game_manager = GameManager(window_width=WINDOW_WIDTH, window_height=WINDOW_HEIGHT, db_file_path=sys.argv[1])
+    game_manager = GameManager(window_width=DISP.get_width(), window_height=DISP.get_height(), db_file_path=sys.argv[1])
     while True:
         handle_events(game_manager)
         render_state(game_manager)
@@ -28,9 +30,8 @@ def main() -> None:
 
 def handle_events(state: GameManager) -> None:
     for event in pygame.event.get():
-        if event.type == pg.QUIT:
-            pygame.quit()
-            sys.exit()
+        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+            shutdown()
         elif event.type == pg.MOUSEBUTTONDOWN:
             state.handle_click(event.pos)
 
@@ -42,6 +43,10 @@ def render_state(state: GameManager) -> None:
         state.mark_rendered()
         pygame.display.update()
         fpsClock.tick(FPS)
+
+def shutdown() -> None:
+    pygame.quit()
+    sys.exit()
 
 
 if __name__ == '__main__':
